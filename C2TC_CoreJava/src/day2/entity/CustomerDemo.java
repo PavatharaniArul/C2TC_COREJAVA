2951c063abd3ebfe378daa56cf35cd29366f4fd3
pavaa <pavaa@PaviArul>
2025-07-22 22:11:20 +0530

    Assignments

---
+++ C2TC_CoreJava/src/day2/entity/CustomerDemo.java
@@ -0,0 +1,7 @@
+package day2.entity;
+
+public class CustomerDemo {
+	public static void main(String[] args) {
+		Customer cl:new Customer();
+		System.out.println(cl);
+		sys
---
+++ C2TC_CoreJava/src/day2/entity/customer.java
@@ -0,0 +1,27 @@
+package day2.entity;
+
+public class customer {
+	private int cid;
+	private String cname;
+	private String city;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+
+}
