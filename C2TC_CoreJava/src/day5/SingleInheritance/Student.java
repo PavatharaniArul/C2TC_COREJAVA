c62266aa66c80deb34aa5b50b5be8112be2b3594
pavaa <pavaa@PaviArul>
2025-07-29 09:55:26 +0530

    day7

---
+++ C2TC_CoreJava/src/day5/SingleInheritance/Student.java
@@ -0,0 +1,61 @@
+package day5.SingleInheritance;
+
+public class Student {
+	private int rollNo;
+	private String collegeName;
+
+	public Student() {
+		super();
+	}
+
+	public Student(String name, String aadharNo, String address, long phno, int rollNo, String collegeName) {
+		super(); //accessing parent class constructor
+		this.rollNo = rollNo;
+		this.collegeName = collegeName;
+	}
+
+	public int getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+
+	public String getCollegeName() {
+		return collegeName;
+	}
+
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [rollNo=" + rollNo + ", collegeName=" + collegeName + ", Name=" + getName()
+				+ ", AadharNo=" + getAadharNo() + ", Address=" + getAddress() + ", Phno=" + getPhno()
+				
+				+ "]";
+	}
+
+	private String getPhno() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String getAddress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String getAadharNo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
