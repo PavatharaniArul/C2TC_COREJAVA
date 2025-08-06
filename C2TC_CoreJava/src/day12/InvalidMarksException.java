2f62ddefbd170cf78178e434319cc004d5584d93
pavaa <pavaa@PaviArul>
2025-08-06 09:52:09 +0530

    Assingment

---
+++ C2TC_CoreJava/src/day12/InvalidMarksException.java
@@ -0,0 +1,15 @@
+package day12;
+
+public class InvalidMarksException extends Exception {
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+	
+}
+
+
